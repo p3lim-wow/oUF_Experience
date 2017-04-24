p3lim-wow/oUF_Experience

commit 771c479dbcc46284fe52586978b2b078e69ef1ed

Prefix the element tags
IMHO, this is best practice when elements add their own tags

oUF_Experience.lua
@@ -3,19 +3,19 @@ local oUF = ns.oUF or oUF
 assert(oUF, 'oUF Experience was unable to locate oUF install')
 
 for tag, func in next, {
-	['curxp'] = function(unit)
+	['experience:cur'] = function(unit)
 		return (IsWatchingHonorAsXP() and UnitHonor or UnitXP) ('player')
 	end,
-	['maxxp'] = function(unit)
+	['experience:max'] = function(unit)
 		return (IsWatchingHonorAsXP() and UnitHonorMax or UnitXPMax) ('player')
 	end,
-	['perxp'] = function(unit)
+	['experience:per'] = function(unit)
 		return math.floor(_TAGS.curxp(unit) / _TAGS.maxxp(unit) * 100 + 0.5)
 	end,
-	['currested'] = function()
+	['experience:currested'] = function()
 		return (IsWatchingHonorAsXP() and GetHonorExhaustion or GetXPExhaustion) ()
 	end,
-	['perrested'] = function(unit)
+	['experience:perrested'] = function(unit)
 		local rested = _TAGS.currested()
 		if(rested and rested > 0) then
 			return math.floor(rested / _TAGS.maxxp(unit) * 100 + 0.5)

commit 6f5be6f7e5dccd4dbacf5c0808a76ac8257be88c

Fix a regression issue from 771c479dbcc46284fe52586978b2b078e69ef1ed
Fixes #6

oUF_Experience.lua
@@ -10,15 +10,15 @@ for tag, func in next, {
 		return (IsWatchingHonorAsXP() and UnitHonorMax or UnitXPMax) ('player')
 	end,
 	['experience:per'] = function(unit)
-		return math.floor(_TAGS.curxp(unit) / _TAGS.maxxp(unit) * 100 + 0.5)
+		return math.floor(_TAGS['experience:cur'](unit) / _TAGS['experience:max'](unit) * 100 + 0.5)
 	end,
 	['experience:currested'] = function()
 		return (IsWatchingHonorAsXP() and GetHonorExhaustion or GetXPExhaustion) ()
 	end,
 	['experience:perrested'] = function(unit)
-		local rested = _TAGS.currested()
+		local rested = _TAGS['experience:currested']()
 		if(rested and rested > 0) then
-			return math.floor(rested / _TAGS.maxxp(unit) * 100 + 0.5)
+			return math.floor(rested / _TAGS['experience:max'](unit) * 100 + 0.5)
 		end
 	end,
 } do